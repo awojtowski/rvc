d05511def6ffd8f2b9cfd5cb985e0437929c84e9
Andy Wojtowski <andy.wojtowski@gmail.com>
2016-12-30 15:22:34 -0600

    Add files via upload

--- front-page.php
+++ front-page.php
@@ -10,4 +10,108 @@
  * @license      GPL-2.0+
  */
 
+add_action('genesis_meta','rvc_homepage_setup');
+
+function rvc_homepage_setup() {
+	$home_sidebars = array(
+		'home-slider' 	   => is_active_sidebar( 'home-slider' ),
+		'home-top'   => is_active_sidebar( 'home-top' ),
+		'home-cta'   => is_active_sidebar( 'home-cta' ),
+		'home-middle'   => is_active_sidebar( 'home-middle' ),
+	);
+
+	// Return early if no sidebars are active.
+	if ( ! in_array( true, $home_sidebars ) ) {
+		return;
+	}
+
+	// Add home welcome area if "Home - Slider" widget area is active.
+	if ( $home_sidebars['home-slider'] ) {
+		add_action( 'genesis_after_header', 'rvc_add_home_slider' );
+	}
+
+	// Add call to action area if "Home - Top" widget area is active.
+	if ( $home_sidebars['home-top'] ) {
+		add_action( 'genesis_after_header', 'rvc_add_home_top' );
+	}
+
+	// Add call to action area if "Home - CTA" widget area is active.
+	if ( $home_sidebars['home-cta'] ) {
+		add_action( 'genesis_after_header', 'rvc_add_home_cta' );
+	}
+
+	// Add call to action area if "Home - Midddle" widget area is active.
+	if ( $home_sidebars['home-middle'] ) {
+		add_action( 'genesis_after_header', 'rvc_add_home_middle' );
+	}
+
+	
+};
+
+
+
+	/**
+	 * Display content for the "Home Slider" section.
+	 *
+	 * @since 1.0.0
+	 */
+	function rvc_add_home_slider() {
+	
+		genesis_widget_area( 'home-slider',
+			array(
+				'before' => '<div class="home-slider"><div class="wrap">',
+				'after' => '</div></div>',
+			)
+		);
+	}
+	
+	/**
+	 * Display content for the "Home Top" section.
+	 *
+	 * @since 1.0.0
+	 */
+	function rvc_add_home_top() {
+	
+		genesis_widget_area( 'home-cta',
+			array(
+				'before' => '<div class="home-cta"><div class="wrap">',
+				'after' => '</div></div>',
+			)
+		);
+		
+	}
+	
+	/**
+	 * Display content for the "Home Call To Action" section.
+	 *
+	 * @since 1.0.0
+	 */
+	function rvc_add_home_cta() {
+	
+		genesis_widget_area( 'home-top',
+			array(
+				'before' => '<div class="home-top"><div class="wrap">',
+				'after' => '</div></div>',
+			)
+		);
+		
+	}
+	
+	/**
+	 * Display content for the "Home Middle" section.
+	 *
+	 * @since 1.0.0
+	 */
+	function rvc_add_home_middle() {
+	
+		genesis_widget_area( 'home-middle',
+			array(
+				'before' => '<div class="home-middle"><div class="wrap">',
+				'after' => '</div></div>',
+			)
+		);
+		
+	}
+
+
 genesis();
--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@
 // Load child theme textdomain.
 load_child_theme_textdomain( 'rvc' );
 
-add_action( 'genesis_setup', 'rvc_setup' );
+add_action( 'genesis_setup', 'rvc_setup', 15 );
 /**
  * Theme setup.
  *
@@ -23,5 +23,45 @@ add_action( 'genesis_setup', 'rvc_setup' );
  * @since 1.0.0
  */
 function rvc_setup() {
+	//* Define Theme Constants
+	define('CHILD_THEME_NAME', 'RVC');
+	define('CHILD_THEME_URI', 'https://github.com/awojtowski/rvc');
+	define('CHILD_THEME_VERSION', '1.0.0');
+	
+	//v Add HTML 5 Markup strucure
+	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption'  ) );
+	//* Add viewport markup for mobile browsers
+	add_theme_support( 'genesis-responsive-viewport' );
+	//* Add theme support for accessibility
+	add_theme_support( 'genesis-accessibility', array(
+		'404-page',
+		'drop-down-menu',
+		'headings',
+		'rems',
+		'search-form',
+		'skip-links',
+	) );
+	//* Add theme support for footer widgets
+	add_theme_support('genesis-footer-widgets', 3);
+	//* Add theme support for custom logo
+	add_theme_support( 'custom-logo', array(
+    'height'      => 100,
+    'width'       => 400,
+    'flex-height' => true,
+    'flex-width'  => true,
+    'header-text' => array( 'site-title', 'site-description' ),
+	) );
+	
+	//* Unregister layout settings
+	genesis_unregister_layout( 'content-sidebar-sidebar' );
+	genesis_unregister_layout( 'sidebar-content-sidebar' );
+	genesis_unregister_layout( 'sidebar-sidebar-content' );
+	
+	//* Unregister secondary sidebar
+	unregister_sidebar( 'sidebar-alt' );
+	
+	//* Add Theme Widgets
+	include_once(get_stylesheet_directory().'/includes/widget-areas.php');
 
+	
 }
