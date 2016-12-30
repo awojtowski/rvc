b7776499cc9b05134420eb0ecfdda06c7a831c78
Andy Wojtowski <andy.wojtowski@gmail.com>
2016-12-30 10:22:38 -0600

    Add files via upload

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Theme customizations
+ *
+ * @package      RVC
+ * Author: Andy Wojtowski
+ * Author URI: http://www.randomthoughtsandstylings.com
+ * @copyright    Copyright (c) 2017, Andy Wojtowski
+ * @license      GPL-2.0+
+ */
+
+genesis();
